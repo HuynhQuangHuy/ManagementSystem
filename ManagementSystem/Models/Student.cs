428afcd4bbf13a87f19d7a8b3c9d65a093f15618
HuynhQuangHuy <buyhuynh123@gmail.com>
2021-02-14 15:28:21 +0700

    Add project files.

---
+++ ManagementSystem/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManagementSystem.Models
+{
+    public class Student
+    { 
+        public int ID { get; set; } //Theo mặc định, Entity Framework diễn giải một thuộc tính có tên IDhoặc tên lớp ID làm khóa chính.
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
